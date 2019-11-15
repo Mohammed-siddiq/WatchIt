7d508cbaea26acf6a2dff8e44d1dda22b6f51dbc
tsodapop <jonathantso93@gmail.com>
2019-11-14 21:41:49 -0600

    Update Call_API.py

--- scripts/Call_API.py
+++ scripts/Call_API.py
@@ -1,15 +1,19 @@
-#need to run the below line in terminal so that we get google application credentials
-#export GOOGLE_APPLICATION_CREDENTIALS="594-a37a08c9cbe2.json 
+#you need to import google-cloud-automl to run the following code
+
+#in terminal, before running the .py file, run the following:
+#export GOOGLE_APPLICATION_CREDENTIALS="CREDENTIALS.json FILE"
+
+#once you are done running the above, the format for running the file is as follows:
+#python call_api.py content, project_id, model_id
+#for us, it will be the below:
+#python call_api.py IMAGE_FILE_PATH 946558125160 ICN6835430693418303488
 
 import os
 import sys
 from google.cloud import automl_v1beta1 as automl
 from google.cloud.automl_v1beta1.proto import service_pb2
 
-os.system('clear')
-print("Console cleared.")
-
-os.system('export GOOGLE_APPLICATION_CREDENTIALS="594-a37a08c9cbe2.json"')
+os.system('clear') #clears the console before running the below
 
 # 'content' is base-64-encoded image data.
 def get_prediction(content, project_id, model_id):
