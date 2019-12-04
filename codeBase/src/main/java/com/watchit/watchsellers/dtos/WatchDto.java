ca012395ea6e7ea2855421b44ba5bf9731c92eca
Mohammed siddiq <iam.mohammedsiddiq@gmail.com>
2019-12-04 13:26:54 -0600

    Updated DTOs

--- codeBase/src/main/java/com/watchit/watchsellers/dtos/WatchDto.java
+++ codeBase/src/main/java/com/watchit/watchsellers/dtos/WatchDto.java
@@ -4,9 +4,37 @@ import lombok.Data;
 
 @Data
 public class WatchDto {
-    private String watchBrand;
+//    private String watchBrand;
+//
+//    private String watchName;
+//
+//    private String watchImage;
 
-    private String watchName;
+    private String brand;
 
-    private String watchImage;
+    private String family;
+
+    private String name;
+
+    private boolean limited;
+
+    private String caseMaterial;
+
+    private String caseBack;
+
+    private String caseShape;
+
+    private String caseDiameter;
+
+    private String dialIndexes;
+
+    private String dialColor;
+
+    private String movementTime;
+
+    private String description;
+
+    private String imageLink;
+
+
 }
